45cbec35321fdf33cbf96009dbe910797c9b3bed
mishfish123 <michelle.yu-ying.wong@monash.edu>
2020-11-04 16:34:47 +1100

    docker modifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,40 @@
-function App() {
-  return <div className="App">Home Page Placeholder.</div>;
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+state = {
+    data: null
+  };
+
+  componentDidMount() {
+      // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Michelle</h1>
+        </header>
+        // Render the newly fetched data inside of this.state.data
+        <p className="App-intro">{this.state.data}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
