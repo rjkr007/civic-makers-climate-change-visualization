aa298b949d20171a5e5a5af952da1a4fba4278c6
Anthony Hien Vu <anthony.vu@team.telstra.com>
2020-11-05 21:39:23 +1100

    Changed front-end to use functional components and remove server/node_modules/* from git repo.

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-class App extends Component {
-state = {
-    data: null
-  };
+const App = () => {
+  const [state, setState] = useState(null);
 
-  componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
+  useEffect(() => {
+    fetch("/test")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setState(result.message);
+        },
+        (error) => {
+          console.warn("Failed to get express msg:", error);
+        }
+      );
+  }, []);
 
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Michelle</h1>
-        </header>
-        // Render the newly fetched data inside of this.state.data
-        <p className="App-intro">{this.state.data}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to Michelle</h1>
+      </header>
+      {state && <p className="App-intro">{state}</p>}
+    </div>
+  );
+};
 
 export default App;
