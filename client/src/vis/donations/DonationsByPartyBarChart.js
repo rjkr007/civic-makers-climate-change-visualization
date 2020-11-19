4fb92022415d5925eae99cc7b76d52490c55f780
Dave Morrissey <dmor0005@student.monash.edu>
2020-11-19 22:31:44 +1100

    cleanups in DonationsByPartyBarChart.js

--- client/src/vis/donations/DonationsByPartyBarChart.js
+++ client/src/vis/donations/DonationsByPartyBarChart.js
@@ -7,75 +7,85 @@ import YearSlider from "../YearSlider";
 import donationsData from "./DonationsData";
 
 
-const getDonationsByParty = () => {
-    let out = {};
-
-    const allowedParties = new Set(['Liberal/National Party', 'Labor Party', 'Palmer United Party', 'KAP', 'One Nation']);
-    const allowedFossilFuelTypes = new Set(['Coal', 'Oil and Gas', 'Coal and Gas', 'All', 'Gas', 'CSG']);
-
-    for (let record of parseCSV(donationsData, {
-        columns: true,
-        skip_empty_lines: true
-    })) {
-        let yearRange = record.Period,
-            partyGroup = record['Party Group'],
-            fossilFuelType = record['Fossil Fuel Type'],
-            value = parseInt(record.Value.replace(/,/g, ''));
-
-        if (!allowedParties.has(partyGroup)) throw "unknown party: "+partyGroup
-        if (!allowedFossilFuelTypes.has(fossilFuelType)) throw "unknown fossil fuel type: "+fossilFuelType
-
-        if (!(yearRange in out)) out[yearRange] = {};
-        if (!(partyGroup in out[yearRange])) out[yearRange][partyGroup] = {};
-        if (!(fossilFuelType in out[yearRange][partyGroup])) out[yearRange][partyGroup][fossilFuelType] = 0;
-
-        out[yearRange][partyGroup][fossilFuelType] += value;
-    }
-    return out;
-}
-
-
 class DonationsByPartyBarChart extends React.Component {
     constructor(props) {
         super(props);
         this.state = { year: 2013 };
+        this.__donationsByParty = this.__getDonationsByParty();
+    }
+
+    __nextAnimationFrame() {
+        if (!this.yearSlider) return;
+        let year = this.yearSlider.getValue()
+
+        if (year !== 2018) {
+            this.yearSlider.setValue(year+1)
+            setTimeout(this.__nextAnimationFrame.bind(this), 500);
+        }
+    }
+
+    /***************************************************
+     * Data Processing
+     ***************************************************/
+
+    __getDonationsByParty() {
+        let out = {};
+
+        const allowedParties = new Set([
+            'Liberal/National Party', 'Labor Party',
+            'Palmer United Party', 'KAP', 'One Nation'
+        ]);
+        const allowedFossilFuelTypes = new Set([
+            'Coal', 'Oil and Gas', 'Coal and Gas',
+            'All', 'Gas', 'CSG'
+        ]);
+
+        for (let record of parseCSV(donationsData, {
+            columns: true,
+            skip_empty_lines: true
+        })) {
+            let yearRange = record['Period'],
+                partyGroup = record['Party Group'],
+                fossilFuelType = record['Fossil Fuel Type'],
+                value = parseInt(record['Value'].replace(/,/g, ''));
+
+            if (!allowedParties.has(partyGroup)) {
+                throw "unknown party: " + partyGroup
+            } else if (!allowedFossilFuelTypes.has(fossilFuelType)) {
+                throw "unknown fossil fuel type: " + fossilFuelType
+            }
+
+            if (!(yearRange in out)) {
+                out[yearRange] = {};
+            }
+            if (!(partyGroup in out[yearRange])) {
+                out[yearRange][partyGroup] = {};
+            }
+            if (!(fossilFuelType in out[yearRange][partyGroup])) {
+                out[yearRange][partyGroup][fossilFuelType] = 0;
+            }
+            out[yearRange][partyGroup][fossilFuelType] += value;
+        }
+        return out;
+    }
+
+    /***************************************************
+     * Template Rendering
+     ***************************************************/
+
+    PARTY_ICONS = {
+        'ALP': process.env.PUBLIC_URL + '/img/alp.jpg',
+        'LNP': process.env.PUBLIC_URL + '/img/lnp.jpeg',
+        'UAP': process.env.PUBLIC_URL + '/img/uap.png',
+        'KAP': process.env.PUBLIC_URL + '/img/kap.png',
+        'OneNation': process.env.PUBLIC_URL + '/img/one_nation.png'
     }
 
     render() {
-        const year = this.state.year;
-        const yearString = `${year - 1}-${year}`
-        const donations = getDonationsByParty();
+        const year = this.state.year,
+              yearString = `${year - 1}-${year}`,
+              donations = this.__donationsByParty;
 
-        const audFormatter = (value) => {
-            let r = value.value.toLocaleString('en-AU', {
-                style: 'currency', currency: 'AUD'
-            }).split('.')[0];
-            return r;
-        }
-        const partyIcons = {
-            'ALP': process.env.PUBLIC_URL + '/img/alp.jpg',
-            'LNP': process.env.PUBLIC_URL + '/img/lnp.jpeg',
-            'UAP': process.env.PUBLIC_URL + '/img/uap.png',
-            'KAP': process.env.PUBLIC_URL + '/img/kap.png',
-            'OneNation': process.env.PUBLIC_URL + '/img/one_nation.png'
-        };
-        const seriesLabel = {
-            normal: {
-                show: true,
-                textBorderColor: '#333',
-                textBorderWidth: 2,
-                formatter: audFormatter
-            }
-        }
-        const getData = (key) => {
-            return [
-                donations[yearString]['Liberal/National Party'][key],
-                donations[yearString]['Labor Party'][key],
-                (donations[yearString]['Palmer United Party'] || {})[key] || null,
-                (donations[yearString]['One Nation'] || {})[key] || null,
-                (donations[yearString]['KAP'] || {})[key] || null,
-            ];
-        }
         const getBars = () => {
             let r = [];
             for (let key of ['Coal', 'Oil and Gas', 'Coal and Gas', 'All', 'Gas', 'CSG']) {
@@ -83,8 +93,21 @@ class DonationsByPartyBarChart extends React.Component {
                     name: key,
                     type: 'bar',
                     stack: true,
-                    data: getData(key),
-                    label: seriesLabel
+                    data: [
+                        donations[yearString]['Liberal/National Party'][key],
+                        donations[yearString]['Labor Party'][key],
+                        (donations[yearString]['Palmer United Party'] || {})[key] || null,
+                        (donations[yearString]['One Nation'] || {})[key] || null,
+                        (donations[yearString]['KAP'] || {})[key] || null,
+                    ],
+                    label: {
+                        normal: {
+                            show: true,
+                            textBorderColor: '#333',
+                            textBorderWidth: 2,
+                            formatter: this.__formatAUDollar
+                        }
+                    }
                 });
             }
             return r;
@@ -94,10 +117,11 @@ class DonationsByPartyBarChart extends React.Component {
                 height: 40,
                 align: 'right',
                 backgroundColor: {
-                    image: partyIcons[key]
+                    image: this.PARTY_ICONS[key]
                 }
             }
         }
+
         const option = {
             title: {
                 text: 'Donations by Fossil Fuel Companies ' + yearString
@@ -142,6 +166,7 @@ class DonationsByPartyBarChart extends React.Component {
             },
             series: getBars()
         };
+
         return <>
             <Chart options={option}
                    style={{
@@ -178,19 +203,15 @@ class DonationsByPartyBarChart extends React.Component {
         </>;
     }
 
+    __formatAUDollar(value) {
+        return value.value.toLocaleString('en-AU', {
+            style: 'currency', currency: 'AUD'
+        }).split('.')[0];
+    }
+
     componentDidMount() {
         // Animate once only!
         setTimeout(this.__nextAnimationFrame.bind(this), 500);
-    }
-
-    __nextAnimationFrame() {
-        if (!this.yearSlider) return;
-        let year = this.yearSlider.getValue()
-
-        if (year !== 2018) {
-            this.yearSlider.setValue(year+1)
-            setTimeout(this.__nextAnimationFrame.bind(this), 500);
-        }
     }
 }
 
