f231fd14004733cd605e2bc7e3935dd11f508af5
mishfish123 <michelle.yu-ying.wong@monash.edu>
2020-12-13 18:14:28 +1100

    last part

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-
 import "./App.css";
 import Container from 'react-bootstrap/Container';
+import {Row, Col} from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => (
@@ -9,11 +9,36 @@ const App = () => (
   <Navbar expand="lg" variant="light" bg="light">
     <Navbar.Brand href="#">Navbar</Navbar.Brand>
   </Navbar>
-  <div class="full-width-image">
-    <img src={ process.env.PUBLIC_URL + '/img/bushfire.jpg' }></img>
-    </div>
-  <Container className="heading">
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </Container>
+    <div>
+  <Container fluid className="however">
+  <Row>
+      <Col className = "nopadding-left" xs = {6} md = {6}>HOWEVER</Col>
+      <Col className = "nopadding-right" xs = {6} md = {6}><div class="h-text">HOWEVER</div></Col>
+    </Row>
+  </Container>
+  <Container fluid className="main call"><Container className="inside"><p>This story <u>should not</u> end with them.</p><p>
+<u>We</u> believe in accountability, transparency and the <br></br>
+right of all people to participate in the<br></br>
+decisions that will shape our future.</p>
+
+
+<p>On this page you will find significant legislation <br></br>
+and motions, debated in the Australian House of<br></br>
+Representatives and Senate and relating to
+climate <br></br> change, carbon emissions and 
+environmental <br></br> protections.</p>
+
+
+<p><u>You</u> will find how your elected officials voted and <br></br>
+<u>their</u> track record of voting on these issues.</p>
+
+
+<p><u>You</u> will find ways that you can make <u>your voice</u>
+<br></br>heard  and ways to make a difference.</p></Container></Container>
+  <Container fluid className="main sep"></Container>
+  <Container fluid className="footer">Footer</Container>
+
+           </div>
   </div>
 );
 
