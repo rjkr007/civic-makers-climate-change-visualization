f13f9a397f6569c943835656640943a951fc1e18
mishfish123 <michelle.yu-ying.wong@monash.edu>
2020-12-15 20:54:32 +1100

    changes to design and added my section

--- client/src/App.js
+++ client/src/App.js
@@ -5,53 +5,83 @@ import "./App.css";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import {Nav, NavItem, NavLink } from 'reactstrap';
+import {Row, Col} from 'react-bootstrap';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <div>
-  <Navbar expand="lg" variant="light" bg="warning">
-    <Navbar.Brand href="#">SeeChanges</Navbar.Brand>
+  <Container fluid className="fluid"><Navbar className = "App-header" expand="lg">
+    <Navbar.Brand className="title" href="#" style={{color: "#C98030"}} >SeeChange.</Navbar.Brand>
     <Nav className = "top-right">
     <NavItem>
-          <NavLink href="#">ABOUT THE DATA</NavLink>
+          <NavLink className="links" href="#" style={{color: "#C98030"}}>ABOUT THE DATA</NavLink>
         </NavItem>
          <NavItem>
-         <NavLink href="#">ABOUT US</NavLink>
-  </NavItem> 
+         <NavLink className="links" href="#" style={{color: "#C98030"}}>ABOUT US</NavLink>
+  </NavItem>
         </Nav>
     </Navbar>
-    <container className = "p1">
-      Be the voice of the change
+    </Container>
+    <Container fluid className = "p1">
+      Be the voice of the change <br></br>
        you want to see around you.
-      </container>
-      <div className = "p2">
-        Find how your elected officals have voted in relation with climate change and make your voice heard 
-      </div>
-      <container className = "p3">
-        Because for more and more Australians climate change is no longer a mere question  of morality.
-      </container>
+       <div className = "p2">
+         <p></p>
+         Find how your elected officals have voted in relation with climate change and <u>make your voice heard.</u>
+       </div>
+      </Container>
 
-     <p className = "p4">
-        It is the very real and physical challenge of 
-        bushfires, floods and droughts causing untold 
-        damage to homes, livelihoods and lives.
-      </p>
+      <Container fluid className="main call"><Container className="inside">
+      <h1 className = "paragraph-headings">
+      Because for more and more <br></br>
+      Australians climate change is <br></br>
+      no longer a mere question  of
+      <br></br>morality.</h1>
 
-      <p className = "p5">
-        It is the country - and the planet - that we will
-         leave to those who come after us.
-      </p>
+      <p>It is the very real and physical challenge of
+      bushfires, floods and droughts causing untold
+      damage to homes, livelihoods and lives.</p>
+      <p>It is the country - and the planet - that we will leave to those who come after us.</p>
+
+      </Container></Container>
 
 
   <div class="full-width-image">
     <img src={ process.env.PUBLIC_URL + '/img/bushfire.jpg' }></img>
-      <div class="centered">It is still our greatest challenge </div>
+      <div class="centered">  <h1 className = "paragraph-headings"> It is still our greatest challenge. </h1></div>
 </div>
+  <div>
+<Container fluid className="however">
+<Row>
+    <Col className = "nopadding-left" xs = {6} md = {6}>HOWEVER</Col>
+    <Col className = "nopadding-right" xs = {6} md = {6}><div class="h-text">HOWEVER</div></Col>
+  </Row>
+</Container>
+<Container fluid className="backgroundforcall call"><Container className="inside"><p>This story <u>should not</u> end with them.</p><p>
+<u>We</u> believe in accountability, transparency and the <br></br>
+right of all people to participate in the<br></br>
+decisions that will shape our future.</p>
 
 
- <Container className="heading">
-      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </Container>  
+<p>On this page you will find significant legislation <br></br>
+and motions, debated in the Australian House of<br></br>
+Representatives and Senate and relating to
+climate <br></br> change, carbon emissions and 
+environmental <br></br> protections.</p>
+
+
+<p><u>You</u> will find how your elected officials voted and <br></br>
+<u>their</u> track record of voting on these issues.</p>
+
+
+<p><u>You</u> will find ways that you can make <u>your voice</u>
+<br></br>heard  and ways to make a difference.</p></Container></Container>
+<Container fluid className="main sep"></Container>
+<Container fluid className="footer">Footer</Container>
+
+         </div>
   </div>
 );
 
